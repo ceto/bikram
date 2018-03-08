4857385fc2432d3517762c81c833646a1cd574c2
Szabó Gábor <szabogabi@gmail.com>
2018-03-08 17:33:51 +0100

    lang excepts

--- templates/calendar-entryclass.php
+++ templates/calendar-entryclass.php
@@ -11,11 +11,11 @@
   </div>
   <?php $teacher=get_field('teacher'); ?>
   <figure class="calcol calentry__teacherpic">
-    <a href="<?= get_the_permalink(get_field('teacher')); ?>">
+    <a href="<?= (ICL_LANGUAGE_CODE=='en')?'#':get_the_permalink( get_field('teacher') ); ?>">
       <?= wp_get_attachment_image($avatar[ID],'thumbnail' ); ?>
     </a>
     <figcaption>
-      <a href="<?= get_the_permalink(get_field('teacher')); ?>">
+      <a href="<?= (ICL_LANGUAGE_CODE=='en')?'#':get_the_permalink(get_field('teacher')); ?>">
         <?= get_field('nick_name', $teacher->ID); ?>
       </a>
     </figcaption>
--- templates/teacher-card.php
+++ templates/teacher-card.php
@@ -1,19 +1,19 @@
 <?php $avatar = get_field('avatar'); ?>
 <div class="tcard">
   <figure class="tcard__photo">
-    <a href="<?php the_permalink(); ?>">
+    <a href="<?= (ICL_LANGUAGE_CODE=='en')?'#':get_the_permalink(); ?>">
       <?= wp_get_attachment_image($avatar[ID], 'thumbnail' ); ?>
     </a>
   </figure>
   <header class="tcard__header">
-    <h3 class="tcard__name"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-    <p class="tcard__quote">
-
-      <?php while ( have_rows('quote') ) : the_row(); ?>
-        <?php the_sub_field('quote_row')?>
-      <?php endwhile; ?>
-
-    </p>
-    <a class="tcard__more" href="<?php the_permalink(); ?>"><?php the_field('nick_name'); ?> <?= __('órái és bemutakozása','bikram') ?>&hellip;</a>
+    <h3 class="tcard__name"><a href="<?= (ICL_LANGUAGE_CODE=='en')?'#':get_the_permalink(); ?>"><?php the_title(); ?></a></h3>
+    <?php if (ICL_LANGUAGE_CODE!=='en') : ?>
+      <p class="tcard__quote">
+        <?php while ( have_rows('quote') ) : the_row(); ?>
+          <?php the_sub_field('quote_row')?>
+        <?php endwhile; ?>
+      </p>
+      <a class="tcard__more" href="<?= (ICL_LANGUAGE_CODE=='en')?'#':get_the_permalink(); ?>"><?php the_field('nick_name'); ?> <?= __('órái és bemutakozása','bikram') ?>&hellip;</a>
+    <?php endif; ?>
   </header>
 </div>
