86025f30932302b56fa2fc77bbf4db0ccb5d3d45
Gabor Szabo <szabogabi@gmail.com>
2020-05-12 16:19:23 +0200

    javitas

--- lib/acffields.php
+++ lib/acffields.php
@@ -919,3 +919,26 @@ acf_add_local_field_group(array (
 ));
 
 endif;
+
+
+// // get all the local field groups
+// $field_groups = acf_get_local_field_groups();
+
+// // loop over each of the gield gruops
+// foreach( $field_groups as $field_group ) {
+
+// 	// get the field group key
+// 	$key = $field_group['key'];
+
+// 	// if this field group has fields
+// 	if( acf_have_local_fields( $key ) ) {
+
+//       	// append the fields
+// 		$field_group['fields'] = acf_get_local_fields( $key );
+
+// 	}
+
+// 	// save the acf-json file to the acf-json dir by default
+// 	acf_write_json_field_group( $field_group );
+
+// }
