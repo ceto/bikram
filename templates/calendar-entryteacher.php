d361411535324c0f488b557c9dd991296351a7ef
Gabor Szabo <szabogabi@gmail.com>
2020-05-12 18:06:26 +0200

    ketfle gomb

--- templates/calendar-entryteacher.php
+++ templates/calendar-entryteacher.php
@@ -10,14 +10,17 @@
     <small> <?= date_i18n('F&\nb\sp;d.', strtotime( get_field('starts') ) ) ?></small>
   </div>
   <div class="calcol calentry__actions">
-<!--     <a href="<?= the_gcaladdlink($post) ?>" target="_blank" rel="nofollow" class="addtocalbtn">
-      <svg viewBox="0 0 512 640" class="icon icon--calendar-plus">
+    <?php if ( get_field('onlineonly', get_field('class') )) : ?>
+        <a style="width:auto; height:auto; padding:0 0.333em 0.25em; line-height:1;" href="https://www.motibro.com/visitors/reservation_widget?operator=bikram-joga-kozpont" target="_blank" rel="nofollow" class="addtocalbtn">
+            <span style="font-size:0.5em; display:inline-block; line-height:2;"><?= _e('JELENTKEZÉS', 'bikram') ?></span>
+        </a>
+      <?php else: ?>
+        <a href="<?php echo the_gcaladdlink($post) ?>" target="_blank" rel="nofollow" class="addtocalbtn">
+        <svg viewBox="0 0 512 640" class="icon icon--calendar-plus">
             <use xlink:href="<?=  get_stylesheet_directory_uri(); ?>/dist/images/icons.svg#icon--calendar-plus"></use>
-      </svg>
-    </a> -->
-    <a style="width:auto; height:auto; padding:0 0.333em 0.25em; line-height:1;" href="https://www.motibro.com/visitors/reservation_widget?operator=bikram-joga-kozpont<?php // echo the_gcaladdlink($post) ?>" target="_blank" rel="nofollow" class="addtocalbtn">
-      <span style="font-size:0.5em; display:inline-block; line-height:2;">JELENTKEZÉS</span>
-    </a>
+        </svg>
+        </a>
+      <?php endif; ?>
   </div>
 </div>
 
