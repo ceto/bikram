a0be67ca593ca8b743be856b6a0ce61c58c75cc1
Gabor Szabo <szabogabi@gmail.com>
2020-05-12 17:34:35 +0200

    favicon added

--- templates/head.php
+++ templates/head.php
@@ -3,6 +3,9 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400i,700|Oswald:300,400,700&subset=latin-ext" rel="stylesheet">
+  <link rel="icon" sizes="192x192" type="image/png" href="<?= get_stylesheet_directory_uri(); ?>/dist/images/favicon/favicon.png">
+  <link rel="icon" sizes="32x32" type="image/png" href="<?= get_stylesheet_directory_uri(); ?>/dist/images/favicon/favicon-32x32.png">
+  <link rel="apple-touch-icon" href="<?= get_stylesheet_directory_uri(); ?>/dist/images/favicon/apple-touch-icon.png">
   <?php wp_head(); ?>
   <?php if ((!defined('WP_ENV') || \WP_ENV === 'production') && !current_user_can('manage_options')) : ?>
   <!-- Global Site Tag (gtag.js) - Google Analytics -->
